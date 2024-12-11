1cea7bc89986a8b74a8c1730f17ab4642d985dc9
ole martin <olesnoen@gmail.com>
2024-12-11 23:55:09 +0100

    export * as Chat

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Logo } from "./components/logo/logo";
-export {Chat} from './components/chat'
-export type  { ChatMessageProps, ChatContextProps} from './components/chat'
+// export {Chat} from './components/chat'
+// export type  { ChatMessageProps, ChatContextProps} from './components/chat'
+export * as Chat from "./components/chat";
